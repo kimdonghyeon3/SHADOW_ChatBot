9a708839c0279984e3ecbba88525c7a48d65fb61
kyj2212 <kyj2212@gmail.com>
2022-08-15 23:04:56 +0900

    Fix: del updating login status

--- src/main/java/com/example/shadow/member/MemberSecurityService.java
+++ src/main/java/com/example/shadow/member/MemberSecurityService.java
@@ -23,20 +23,28 @@ public class MemberSecurityService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String memberId) throws UsernameNotFoundException {
-       Optional<Member> _member= this.memberRepository.findByMemberId(memberId);
+        Member member = findByMemberId(memberId);
+        List<GrantedAuthority> authorities = getAuthorities(member);
+        return new User(member.getMemberId(), member.getMemberPwd(), authorities);
+    }
+
+    private Member findByMemberId(String memberId){
+        Optional<Member> _member= this.memberRepository.findByMemberId(memberId);
         if (_member.isEmpty()) {
             throw new UsernameNotFoundException("사용자를 찾을수 없습니다.");
         }
-        Member member = _member.get();
+        return _member.get();
+    }
+    private List<GrantedAuthority> getAuthorities(Member member){
+        String memberId=member.getMemberId();
         List<GrantedAuthority> authorities = new ArrayList<>();
         if ("admin".equals(memberId)) {
             authorities.add(new SimpleGrantedAuthority(MemberRole.ADMIN.getValue()));
         } else {
             authorities.add(new SimpleGrantedAuthority(MemberRole.MEMBER.getValue()));
-            member.setMemberLoginStatus(true);
             this.memberRepository.save(member);
         }
-        return new User(member.getMemberId(), member.getMemberPwd(), authorities);
-
+        return authorities;
     }
+
 }
