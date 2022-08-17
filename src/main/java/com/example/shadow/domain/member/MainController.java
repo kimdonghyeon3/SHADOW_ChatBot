5b234238779c213c7ce87515f4e892d295595a23
kyj2212 <kyj2212@gmail.com>
2022-08-18 00:09:51 +0900

    Fix: root path index page

---
+++ src/main/java/com/example/shadow/domain/member/MainController.java
@@ -0,0 +1,14 @@
+package com.example.shadow.domain.member;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+    @RequestMapping("/")
+    public String root() {
+        return "index";
+    }
+
+}
