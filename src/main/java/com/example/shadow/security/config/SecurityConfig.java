b834b620309d94853de2593f778fd59054c6c615
kyj2212 <kyj2212@gmail.com>
2022-08-18 13:47:53 +0900

    Fix: configure -> Bean, caused deprecated websecurityconfigureadapter

--- src/main/java/com/example/shadow/security/config/SecurityConfig.java
+++ src/main/java/com/example/shadow/security/config/SecurityConfig.java
@@ -1,44 +1,42 @@
 package com.example.shadow.security.config;
 
-import com.example.shadow.security.handler.CustomFailureHandler;
 import com.example.shadow.security.handler.CustomSuccessHandler;
 import com.example.shadow.security.service.MemberSecurityService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-
 @RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 
     /* 인가 구분을 위한 url path 지정 */
-    private static final String[] AUTH_WHITELIST_STATIC = {"/css/**", "/js/**", "/assets/**", "/error"}; // 정적 파일 인가 없이 모두 허용
-    private static final String[] AUTH_ALL_LIST = {"/singup/**", "/login/**"}; // 모두 허용
-    private static final String[] AUTH_ADMIN_LIST = {"/admin"}; // admin 롤 만 허용
-    private static final String[] AUTH_AUTHENTICATED_LIST = {"/members/**", "/flowcharts/**", "/main/**"}; // 인가 필요
+    private static final String[] AUTH_WHITELIST_STATIC = {
+            "/css/**",
+            "/js/**",
+            "/assets/**",
+            "/error/**"}; // 정적 파일 인가 없이 모두 허용
+    private static final String[] AUTH_ALL_LIST = {
+            "/singup/**",
+            "/login/**"}; // 모두 허용
+    private static final String[] AUTH_ADMIN_LIST = {
+            "/admin/**"}; // admin 롤 만 허용
+    private static final String[] AUTH_AUTHENTICATED_LIST = {
+            "/members/**",
+            "/flowcharts/**",
+            "/main/**"}; // 인가 필요
 
     private final MemberSecurityService customUserDetailsService;
     private final AuthenticationFailureHandler customFailureHandler;
@@ -61,13 +59,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return new CustomSuccessHandler("/");
     }
 
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers(AUTH_WHITELIST_STATIC);
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() throws Exception {
+       return (web) -> web.ignoring().antMatchers(AUTH_WHITELIST_STATIC);
     }
 
-    @Override
-    public void configure(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
             .authorizeRequests()
                 .antMatchers(AUTH_ALL_LIST).permitAll()
@@ -92,12 +90,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .exceptionHandling()
                 .accessDeniedPage("/restrict");
         ;
+        return http.build();
     }
-
-    @Override
-    public void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.authenticationProvider(daoAuthenticationProvider());
-    }
-
 
 }
