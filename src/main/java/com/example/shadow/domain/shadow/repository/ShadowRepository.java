b4a1d5ce85e68c71b73ed8a7f8e19f17ab05b6f6
jiooong <lucy991010@gmail.com>
2022-08-29 10:40:11 +0900

    Chore: Modify ShadowRepository class to interface

--- src/main/java/com/example/shadow/domain/shadow/repository/ShadowRepository.java
+++ src/main/java/com/example/shadow/domain/shadow/repository/ShadowRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class ShadowRepository extends JpaRepository<Shadow, Long> {
+public interface ShadowRepository extends JpaRepository<Shadow, Long> {
 
 
 }
