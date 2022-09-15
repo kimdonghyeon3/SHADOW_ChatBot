1942cf0c71fb550ca3734de3d5a0f8d5cd948a8d
kyj2212 <kyj2212@gmail.com>
2022-09-15 23:33:22 +0900

    Chore: for deploy modify

--- src/main/java/com/example/shadow/ShadowApplication.java
+++ src/main/java/com/example/shadow/ShadowApplication.java
@@ -2,15 +2,19 @@ package com.example.shadow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.filter.HiddenHttpMethodFilter;
 
 @SpringBootApplication
 public class ShadowApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(ShadowApplication.class, args);
 	}
+
 	@Bean
 	public HiddenHttpMethodFilter hiddenHttpMethodFilter(){
 		return new HiddenHttpMethodFilter();
